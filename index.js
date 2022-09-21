fb2a46e7197087fde8edbd1207e92e7f9ccf72ab
celestino <celestino.martinez01@gmail.com>
2022-09-21 16:28:36 -0400

    commit tree 2ca64e8438d3ca7725385a3851c4c080f783a879

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Celestino Martinez",
+    streetAddress: "1700 Crotona Park East"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const copyEmployee = {...obj}
+    copyEmployee[key] = value
+    return copyEmployee
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj.key = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const copyEmployee = {...employee}
+    delete copyEmployee[key]
+    return copyEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
